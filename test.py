2235ee5a1fcca8b23f4ae176b40622c316dc09b7
MarcosAndradeV <mastermarcos1212@hotmail.com>
2023-12-29 08:49:06 -0300

    update tests

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import os, subprocess
 
+if not os.path.exists("./tmp/chsvm"):
+    print("Please use: make chsvm")
+    exit(0)
+
 directory = './tests'
 for filename in os.listdir(directory):
     if filename.endswith('.chs') and "chsvm" in os.listdir("./tmp"):
