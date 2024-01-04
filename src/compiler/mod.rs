7f4837f69cddc7d3aea7936d115d50b195c7b5e3
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-01-04 15:06:15 -0300

    rewriting the compiler

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,20 +1,3 @@
-use std::path::PathBuf;
-
-use crate::{exepitons::GenericError, generic_error};
-
-use self::{node::Program, parser::{ParseError, Parser}};
-
-
-
 pub mod lexer;
-pub mod node;
+pub mod ast;
 pub mod parser;
-
-
-pub fn make_ast(source: String) -> Result<Program, GenericError> {
-    let mut parser = Parser::new(source.into_bytes(), PathBuf::new());
-    match parser.parse() {
-        Ok(ok) => Ok(ok),
-        Err(p) => generic_error!("Cannot parser the file: {}", p.message),
-    }
-}
