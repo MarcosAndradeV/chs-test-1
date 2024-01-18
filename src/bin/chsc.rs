18715d4bbe5c249a69f0ab3b7498d1fff5994f65
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-01-18 10:55:53 -0300

    add type check flag

--- src/bin/chsc.rs
+++ src/bin/chsc.rs
@@ -1,6 +1,6 @@
-use std::io;
+use std::{io, process};
 
-use chs::{lex_file, vm::CHSVM, compiler::parser::Parser};
+use chs::{lex_file, vm::CHSVM, compiler::{parser::Parser, type_checker::type_check_program}};
 use clap::{Arg, Command, ArgAction};
 
 fn main() -> io::Result<()>{
@@ -14,13 +14,21 @@ fn main() -> io::Result<()>{
         .subcommand(
             Command::new("run")
                 .about("...")
+                .short_flag('R')
                 .arg(Arg::new("filename").value_name("FILE").num_args(1))
                 .arg(
                     Arg::new("debug")
                         .long("debug")
                         .short('d')
                         .action(ArgAction::SetTrue)
-                        .help("Runs with debug mode")),
+                        .help("Runs with debug mode"))
+                .arg(
+                    Arg::new("check")
+                        .long("check")
+                        .short('c')
+                        .action(ArgAction::SetTrue)
+                        .help("Runs with type check mode")),
+                
 
         )
         .get_matches();
@@ -36,12 +44,17 @@ fn main() -> io::Result<()>{
                     Ok(o) => o,
                     Err(e) => {eprintln!("{}", e); return Ok(())}
                 };
-                let mut vm = CHSVM::new(code);
-                if file_matches.get_flag("debug") {
-                    vm.run(true);
-                    return Ok(());
+                if file_matches.get_flag("check") {
+                    match type_check_program(&code) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            process::exit(1);
+                        }
+                    }
                 }
-                vm.run(false);
+                let mut vm = CHSVM::new(code);
+                vm.run(file_matches.get_flag("debug"));
                 return Ok(());
             }
             println!("File not provided.");
