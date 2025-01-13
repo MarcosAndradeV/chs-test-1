2e40e9edc0379e2938499662cd2481e64d114607
MarcosAndradeV <mastermarcos1212@hotmail.com>
2025-01-13 07:59:36 -0300

    Update CLI

--- chs/src/main.rs
+++ chs/src/main.rs
@@ -1,15 +1,27 @@
-use std::{env, fs};
+#![allow(unused)]
 
-use chs_ast::Parser;
-use chs_lexer::Lexer;
+use std::{env, process::exit};
 
 fn main() {
-    let mut args = env::args();
-    let _program = args.next().expect("Expect program");
-    let file = args.next().expect("Expect file");
-    let src = fs::read(&file)
-        .expect("Failed to read file");
-    let lex = Lexer::new(file.into(), src);
-    let module = Parser::new(lex).parse().expect("Parsing falied");
-    println!("{}", module)
+    let mut argv = env::args();
+    let program = argv.next().expect("Program always provided.");
+    let cmd = argv.next().unwrap_or_else(|| usage_err(&program, "Expect Command"));
+    match cmd.as_str() {
+        "help" => usage(&program),
+        "com"|"compile" => {}
+        c => usage_err(&program, format!("Invalid Command \"{c}\""))
+    }
+}
+
+fn usage(program: &str) {
+    println!("USAGE: {program} <COMMAND> [OPTIONS]");
+    println!("COMMANDS:");
+    println!("    help - Show this message.");
+    println!("    com|compile - Compile a program: chs com <file.chs>");
+}
+
+fn usage_err(program: &str, err: impl ToString) -> ! {
+    eprintln!("ERROR: {}", err.to_string());
+    usage(&program);
+    exit(1);
 }
